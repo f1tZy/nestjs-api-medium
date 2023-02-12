034759687aff4d20d4e35ef8ebe846771a026ada
f1tzy <winndini@gmail.com>
2023-02-12 23:28:08 +0300

    fix testing & package json

--- src/tag/tag.controller.spec.ts
+++ src/tag/tag.controller.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TagController } from './tag.controller';
+import { TagController } from '@app/tag/tag.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormOptions } from '@app/db/orm-options';
+import { TagEntity } from '@app/tag/tag.entity';
 
 describe('TagController', () => {
   let controller: TagController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(ormOptions), TypeOrmModule.forFeature([TagEntity])],
       controllers: [TagController],
     }).compile();
 
--- src/tag/tag.service.spec.ts
+++ src/tag/tag.service.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TagService } from './tag.service';
+import { TagService } from '@app/tag/tag.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagEntity } from '@app/tag/tag.entity';
+import { ormOptions } from '@app/db/orm-options';
 
 describe('TagService', () => {
   let service: TagService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(ormOptions), TypeOrmModule.forFeature([TagEntity])],
       providers: [TagService],
     }).compile();
 
